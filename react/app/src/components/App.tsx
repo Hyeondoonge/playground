78c9c36cb6e5eaba72a033f0f1539895474b7f5e
Hyeondoonge <migu554@naver.com>
2023-10-09 02:05:18 +0900

    리팩토링- Product 관련 관심사 분리

--- react/app/src/components/App.tsx
+++ react/app/src/components/App.tsx
@@ -1,11 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import ErrorBoundary from './ErrorBoundary';
-
-interface Product {
-  title: string;
-  count?: number;
-  introduction: string;
-}
+import Product, { ProductFallback } from './Product';
 
 export default function App() {
   return (
@@ -13,47 +8,6 @@ export default function App() {
       <ErrorBoundary fallback={<ProductFallback />}>
         <Product />
       </ErrorBoundary>
-    </div>
-  );
-}
-
-function ProductFallback() {
-  return <div>💥 에러가 발생했어요</div>;
-}
-
-function Product() {
-  const [product, setProduct] = useState<Product | null>(null);
-  const [error, setError] = useState<boolean>(false);
-
-  useEffect(() => {
-    const fetchProduct = async () => {
-      try {
-        const URL = 'https://dummyjson.com/products/1';
-        const res = await fetch(URL);
-        if (!res.ok) throw new Error('무튼 에러가 발생했어요');
-
-        const product: Product = await res.json();
-
-        if (product.title !== 'iPhone 9') {
-          setError(true);
-          return;
-        }
-        setProduct(product);
-      } catch (error: unknown) {
-        setError(true);
-      }
-    };
-
-    fetchProduct();
-  }, []);
-
-  if (error) {
-    throw new Error();
-  }
-
-  return (
-    <div>
-      {product?.title} {product?.introduction}
     </div>
   );
 }
---
+++ react/app/src/components/Product.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+
+interface Product {
+  title: string;
+  count?: number;
+  introduction: string;
+}
+
+export function ProductFallback() {
+  return <div>💥 에러가 발생했어요</div>;
+}
+
+export default function Product() {
+  const [product, setProduct] = useState<Product | null>(null);
+  const [error, setError] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const URL = 'https://dummyjson.com/products/1';
+        const res = await fetch(URL);
+        if (!res.ok) throw new Error('무튼 에러가 발생했어요');
+
+        const product: Product = await res.json();
+
+        if (product.title !== 'iPhone 9') {
+          setError(true);
+          return;
+        }
+        setProduct(product);
+      } catch (error: unknown) {
+        setError(true);
+      }
+    };
+
+    fetchProduct();
+  }, []);
+
+  if (error) {
+    throw new Error();
+  }
+
+  return (
+    <div>
+      {product?.title} {product?.introduction}
+    </div>
+  );
+}

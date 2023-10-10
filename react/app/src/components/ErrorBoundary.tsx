89af837e9f8b282699a765d5170401296cdbf43d
Hyeondoonge <migu554@naver.com>
2023-10-11 01:34:53 +0900

    ErrorBoundary 컴포넌트 부연설명 추가

--- react/app/src/components/ErrorBoundary.tsx
+++ react/app/src/components/ErrorBoundary.tsx
@@ -15,13 +15,17 @@ export default class ErrorBoundary extends React.Component<IProps, IState> {
     this.state = { hasError: false }; // 에러 상태 지정
   }
 
-  static getDerivedStateFromError(error: Error) {
+  static getDerivedStateFromError(error: unknown) {
     // 에러 발생 시, 상태를 변경하여 이에 따른 fallback UI를 렌더링한다
+    // error는 컴포넌트에서 던진 에러
     return { hasError: true };
   }
 
   componentDidCatch(error: unknown, info: ErrorInfo) {
-    // optinal한 구현사항이다, 에러 발생 시 원하는 작업을 작성하면된다
+    // 이 함수는 optinal한 구현사항이다, 에러 발생 시 원하는 작업을 작성하면된다
+
+    // error는 컴포넌트에서 던진 에러
+
     // 에러 로그 정보 출력 예시
     // Example "componentStack":
     //   in ComponentThatThrows (created by App)
